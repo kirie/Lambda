854480bcbabf04c86a78067ba6d005965226c339
Eirik <kirie@users.noreply.github.com>
2019-01-29 20:46:27 -0800

    Add - Constants and setup

--- LowUtilization2.js
+++ LowUtilization2.js
@@ -6,3 +6,15 @@
     3. Iterate over the instances in step 1 and attach their tags taken from step 2
     4. Send emails using SES
 **/
+
+// Your own constants
+const tableName = 'LowUtilTable';
+const awsRegion = 'us-west-2';
+const emailSource = "you@email.com"; // verified
+const emailTo = ["person@owner.com"];  // array of addresses
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: awsRegion
+})
