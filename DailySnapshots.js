8b0dd5357228eab3041a9ea74cd136e31aa43a59
Eirik <kirie@users.noreply.github.com>
2019-02-22 09:34:39 -0800

    Edit - Date Formatting

--- DailySnapshots.js
+++ DailySnapshots.js
@@ -76,3 +76,7 @@ function tagSnapshot(snapshotId, deleteOn) {
     }
   });
 }
+
+function formatDate(date) {
+  return date.getFullYear() + '' + ('0' + (date.getMonth()+1)).slice(-2) + '' + ('0' + date.getDate()).slice(-2);
+}
