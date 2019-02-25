6d2549b9bd55f7e764cb5ea47aad0b14fbc29227
Eirik <kirie@users.noreply.github.com>
2019-02-25 08:58:06 -0800

    Add - Template start

---
+++ SESTemplate.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const TemplateName = "myTemplate";
+const Subjectpart = "My Subject Title";
+
+AWS.config.update({
+  region: 'us-west-2'
+})
+
+// Load new 
+const readFile = filePath => new Promise((resolve, reject) => {
+  fs.readFile('./email.html', 'utf8', (err, data) => {
+    if (err) reject(err);
+    else resolve(data)
+  })
+})
+
+async function makeTemplate() {
+  const SES = new AWS.SES({
+    apiVersion: '2010-12-01'
+  });
+
+  // read the email File
+
+  const templateParams = {
+    Template: {
+      TemplateName,
+      SubjectPart,
+      HtmlPart: await readFile()
+    }
+  }
+
+  let makeT = await SES.createTemplate(templateParams).promise();
+  console.log('Updated Template', makeT)
+}
