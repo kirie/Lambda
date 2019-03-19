01a2f7e1ef4f59e89fd5de0f748d07f1e4081c6e
Eirik <kirie@users.noreply.github.com>
2019-03-19 12:43:19 -0700

    Edit - Cleanup

--- SiteKeepCached.js
+++ SiteKeepCached.js
@@ -1,19 +1,19 @@
 const nodeFetch = require('node-fetch');
 
-let sites = ['https://www.httpstest.com', 'http://www.httptest.com'];
+let sites = ['https://www.google.com', 'http://www.gogle.com'];
 
 exports.handler = async (event, context, callback) => {
   try {
     const promises = sites.map(myFetch);
     let status = await Promise.all(promises);
-    console.log('Status: ', status.every(v => v === 200))
+    console.log('Status: ', status.every(v => v === 200));
   }
   catch (err) {
-    callback(err.message)
+    callback(err.message);
   }
 }
 
 async function myFetch(url) {
   return nodeFetch(url).then(x => x.status)
-  //return nodeFetch(url, {redirect: 'error'}  if dont want redirects
+  //return nodeFetch(url, {redirect: 'error'}).then(x => x.status)  no redirects
 }
