305d0898fbe0d446d157b50e384ab41694161421
Eirik <kirie@users.noreply.github.com>
2019-07-10 14:45:21 -0700

    Add - Processing Function

--- XLSconversion.js
+++ XLSconversion.js
@@ -25,5 +25,45 @@ exports.handler = async (event, context, callback) => {
 
 async function excel () {
   const sorted = {};
-  const workbook = XLSX
+  const workbook = XLSX.readFile(excelFile);
+  const firstSheet = workbook.SheetNames[0];
+  const theExcel = workbook.Sheets[firstSheet];
+  const arrExcel = sheet2arr(theExcel);
+
+  // Verify the total and numbers
+  if (verifyTotal(arrExcel)) {
+    const items = arrExcel.slice(0, arrExcel.length - 1);
+
+    // Split the items by date and insert into 1 Object
+    items.forEach((v) => {
+      sorted[v[4]] = sorted[v[4]] ? [...sorted[v[4]], v] : [v];
+    });
+
+    // iterate over the object for items sorted by date paid
+    Object.entries(sorted).forEach(([key, value]) => {
+      processData(key, value);
+    });
+  }
+}
+
+
+function processData(key, val) {
+  const startDate = moment(key, 'MM/DD/YY').format('YYYYMMDD');
+  const stream = fs.createWriteStream(`${startDate}Wells`);
+
+  const begin = HEADERstr + startDate;
+  stream.write(`${begin}\r\n`); // Add windows CRLF
+
+  const shortDate = startDate.slice(2);
+
+  val.forEach((v) => {
+    const amountPaid = removeCommas(v[3]);
+    const padded = padEleven(removeDecimal(amountPaid));
+    const line = ACCOUNT + shortDate + v[0] + SPACEPAD1 + padded + SPACEPAD2 + startDate;
+
+    stream.write(`${line}\r\n`);
+    console.log(line);
+  });
+
+  stream.end();
 }
